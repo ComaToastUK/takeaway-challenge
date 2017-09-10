b0e63df93f2f21fa5c4b612dd7a0f8f71fb7cc04
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-09-10 14:18:44 +0100

    added an order class

---
+++ lib/order.rb
@@ -0,0 +1,9 @@
+class Order
+
+attr_reader :order
+
+  def initialize(input)
+    @order = input
+  end
+
+end
---
+++ spec/order_spec.rb
@@ -0,0 +1,7 @@
+require 'order'
+
+RSpec.describe Order do
+  it 'allows a user to input their order' do
+
+  end
+end
